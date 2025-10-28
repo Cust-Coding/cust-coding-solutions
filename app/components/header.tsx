185d0defd07f1739284bb654b83c47b2ce966024
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-10-28 18:35:42 -0500

    feat: add layer blur in navegation bar

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,19 +9,22 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 right-0 w-screen  m-0 p-0 py-4 flex items-center justify-center z-50">
+      <div className="menu relative z-40 flex items-center justify-center my-2 border-1 border-dashed rounded-xl py-4 w-[94%] md:w-[calc(100%-30%)]">
+        <div className=" w-full p-2 backdrop-blur-[0.625rem] absolute h-full bg-gray-500/20 rounded-xl"></div>
       
-      <div className="menu flex items-center justify-around my-2 border-1 border-dashed rounded-xl py-4 w-[94%] md:w-[calc(100%-30%)]">
-        <Link href="/" className="">
-          <Image
-            src={"/logoccs.svg"}
-            alt={logocss[0].title}
-            width={120}   
-            height={50}  
-            priority
-          />
-      </Link>
+        <div className="flex z-40 items-center justify-around w-full">
+          <Link href="/" className="">
+            <Image
+              src={"/logoccs.svg"}
+              alt={logocss[0].title}
+              width={120}   
+              height={50}  
+              priority
+            />
+        </Link>
 
-        <Nav/>
+          <Nav/>
+        </div>
       </div>
 
     </header>
